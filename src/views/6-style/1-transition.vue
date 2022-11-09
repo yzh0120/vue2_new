5ed7ca9544d050d4ef7d3adf2a82b21c218362b8
yzh0120 <644193385@qq.com>
2022-11-09 16:48:20 +0800

    up

--- src/views/6-style/1-transition.vue
+++ src/views/6-style/1-transition.vue
@@ -1,7 +1,15 @@
+<!--
+ * @Author: yz
+ * @Date: 2022-11-09 10:15:12
+ * @Description: 
+ * 
+-->
 <template>
-  <div>
+  <page>
+    <div class="transition"></div>
 
-  </div>
+    <p>鼠标移动到 div 元素上，查看过渡效果。</p>
+  </page>
 </template>
 
 <script>
@@ -11,5 +19,14 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.transition {
+  width: 100px;
+  height: 100px;
+  background: red;
+  transition: width 2s;
 
+  &:hover {
+    width: 300px;
+  }
+}
 </style>
